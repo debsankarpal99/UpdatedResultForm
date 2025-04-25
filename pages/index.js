190d02fea5e7d87e8195dce77a9aec8367dcb88c
Debsankar Pal <debsankarpal99@gmail.com>
2025-04-25 12:46:04 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,13 @@
-.
+// pages/index.js
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+export default function Home() {
+  const router = useRouter();
+  
+  useEffect(() => {
+    router.push('/form/1');
+  }, [router]);
+  
+  return <div>Redirecting to form...</div>;
+}
